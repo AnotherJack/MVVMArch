765e99ee68f9eaf0ba24598794cca558255bbb05
zhengj <x614545287@gmail.com>
2018-02-07 17:26:43 +0800

    developing

--- app/src/main/java/io/github/anotherjack/mvvmarchdemo/mvvm/model/entity/User.java
+++ app/src/main/java/io/github/anotherjack/mvvmarchdemo/mvvm/model/entity/User.java
@@ -5,37 +5,37 @@ package io.github.anotherjack.mvvmarchdemo.mvvm.model.entity;
  */
 
 public class User {
-    private int id;
-    private String login;
-    private String avatar_url;
+    private String name;
+    private int age;
+    private String address;
 
-    public User(int id, String login, String avatar_url) {
-        this.id = id;
-        this.login = login;
-        this.avatar_url = avatar_url;
+    public User(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
     }
 
-    public int getId() {
-        return id;
+    public String getName() {
+        return name;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getLogin() {
-        return login;
+    public int getAge() {
+        return age;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    public String getAvatar_url() {
-        return avatar_url;
+    public String getAddress() {
+        return address;
     }
 
-    public void setAvatar_url(String avatar_url) {
-        this.avatar_url = avatar_url;
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
