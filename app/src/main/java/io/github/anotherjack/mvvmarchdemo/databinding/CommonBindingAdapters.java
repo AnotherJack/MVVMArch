7d12c601bbd004bad4b69d84133ee9ecba3448e3
zhengj <x614545287@gmail.com>
2018-02-09 15:22:27 +0800

    developing

--- app/src/main/java/io/github/anotherjack/mvvmarchdemo/databinding/CommonBindingAdapters.java
+++ app/src/main/java/io/github/anotherjack/mvvmarchdemo/databinding/CommonBindingAdapters.java
@@ -2,6 +2,10 @@ package io.github.anotherjack.mvvmarchdemo.databinding;
 
 import android.databinding.BindingAdapter;
 import android.databinding.InverseBindingAdapter;
+import android.databinding.InverseBindingListener;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
@@ -19,4 +23,38 @@ public class CommonBindingAdapters {
 //        String str = view.getText().toString();
 //        return Integer.parseInt(str);
 //    }
+
+    @BindingAdapter("txt")
+    public static void setTxt(TextView textView, String value){
+        String oldValue = textView.getText().toString();
+        if(value.equals(oldValue)) return;
+        Log.d("BindingAdapter","setTxt");
+        textView.setText(value);
+    }
+
+    @BindingAdapter("ttxtAttrChanged")
+    public static void setTxtWatcher(TextView textView, final InverseBindingListener txtAttrChanged){
+        textView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                txtAttrChanged.onChange();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @InverseBindingAdapter(attribute = "txt",event = "ttxtAttrChanged")
+    public static String getTxtString(TextView textView){
+        Log.d("InverseBindingAdapter","getTxtString");
+        return textView.getText().toString();
+    }
 }
