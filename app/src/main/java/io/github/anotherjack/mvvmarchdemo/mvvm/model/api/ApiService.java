2213bd54d7321ddaa22234cbc977d6aa162db375
zhengj <x614545287@gmail.com>
2018-03-12 16:42:06 +0800

    developing

--- app/src/main/java/io/github/anotherjack/mvvmarchdemo/mvvm/model/api/ApiService.java
+++ app/src/main/java/io/github/anotherjack/mvvmarchdemo/mvvm/model/api/ApiService.java
@@ -1,12 +1,5 @@
 package io.github.anotherjack.mvvmarchdemo.mvvm.model.api;
 
-import java.util.List;
-
-import io.github.anotherjack.mvvmarchdemo.mvvm.model.entity.User;
-import io.reactivex.Observable;
-import retrofit2.http.GET;
-import retrofit2.http.Headers;
-import retrofit2.http.Query;
 
 /**
  * Created by jack on 2018/2/2.
@@ -14,9 +7,4 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    String HEADER_API_VERSION = "Accept: application/vnd.github.v3+json";
-
-    @Headers({HEADER_API_VERSION})
-    @GET("/users")
-    Observable<List<User>> getUsers(@Query("since") int lastIdQueried, @Query("per_page") int perPage);
 }
